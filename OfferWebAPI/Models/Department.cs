e6cb7b79f7dfce320b5d71b31bd8483f24335592
NikolajManssonEsSolutions <nikolaj.mansson@essolutions.se>
2022-01-24 17:26:13 +0100

    Added Service and Department models

---
+++ OfferWebAPI/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace OfferWebAPI.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public decimal SQMPrice { get; set; }
+    }
+}
---
+++ OfferWebAPI/Models/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace OfferWebAPI.Models
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public decimal Price { get; set; }
+        public string Department { get; set; }
+
+    }
+}
