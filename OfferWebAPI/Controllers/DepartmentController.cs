0a2910943bf851d2b7f2d7542978209d8fa801b8
NikolajManssonEsSolutions <nikolaj.mansson@essolutions.se>
2022-01-26 15:15:15 +0100

    Added delete REST API to department

--- OfferWebAPI/Controllers/DepartmentController.cs
+++ OfferWebAPI/Controllers/DepartmentController.cs
@@ -94,5 +94,31 @@ namespace OfferWebAPI.Controllers
 
             return new JsonResult("Updated successfully");
         }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from dbo.Department where
+                    DepartmentId = " + id + @"
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted successfully");
+        }
     }
 }
