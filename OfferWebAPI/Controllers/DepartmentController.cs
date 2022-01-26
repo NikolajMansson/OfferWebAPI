ea99c989c0de4ec43e6dc672869358b9639182fc
NikolajManssonEsSolutions <nikolaj.mansson@essolutions.se>
2022-01-26 15:13:45 +0100

    Added put-method to department

--- OfferWebAPI/Controllers/DepartmentController.cs
+++ OfferWebAPI/Controllers/DepartmentController.cs
@@ -67,5 +67,32 @@ namespace OfferWebAPI.Controllers
 
             return new JsonResult("Added successfully");
         }
+
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            string query = @"
+                    update dbo.Department set
+                    DepartmentName = '" + dep.DepartmentName + @"', SQMPrice = ROUND(" + dep.SQMPrice + @", 2)
+                    where DepartmentId = " + dep.DepartmentId + @"                    
+";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated successfully");
+        }
     }
 }
